b1d820e12b5d2354f555a2f9694123ce6e8cf188
opalenzuela <opalenzuela@gmail.com>
2014-07-22 12:48:50 +0200

    Build scripts updated

--- mkpkg.sh
+++ mkpkg.sh
@@ -2,10 +2,8 @@
 
 mkdir -p pkg
 rm pkg/*.tar.gz 2>/dev/null
-PLUGIN="odnetdrives"
-VERSION=`cat var/opendomo/plugins/odnetdrives.version`
+PKGID="odnetdrives"
 USR="--owner 1000 --group 1000 --same-permissions"
 EXCLUDE=' --exclude "*~" --exclude .svn --exclude README.md --exclude LICENCE'
 
-rm -fr pkg/*.tar.gz
-tar zcf  pkg/$PLUGIN-$VERSION.tar.gz usr var  $USR $EXCLUDE
+tar cvfz $PKGID-`date '+%Y%m%d'`.tar.gz usr var --owner 1000 --group 1000 --exclude "*~" --exclude .svn --exclude "*.a"
