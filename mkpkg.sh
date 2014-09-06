730bafea046d9934e5d85b72bedead3cd095c6d1
Javier Miras Blanco <jmirasb@gmail.com>
2014-09-06 11:19:31 +0200

    Updating mkpkg.sh

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-
-mkdir -p pkg
-rm pkg/*.tar.gz 2>/dev/null
 PKGID="odnetdrives"
-USR="--owner 1000 --group 1000 --same-permissions"
-EXCLUDE=' --exclude "*~" --exclude .svn --exclude README.md --exclude LICENCE'
-
 tar cvfz $PKGID-`date '+%Y%m%d'`.tar.gz usr var --owner 1000 --group 1000 --exclude "*~" --exclude .svn --exclude "*.a"
