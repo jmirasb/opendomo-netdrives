6d0ced3c108119840b39cb874bd5828422d2405e
root <root@OpenDomoOS.(none)>
2014-09-12 08:22:52 +0200

    Share samba drives

--- usr/local/opendomo/eventhandlers/addsharedrive.sh
+++ usr/local/opendomo/eventhandlers/addsharedrive.sh
@@ -8,23 +8,43 @@
 # Is necessary device name
 if ! test -z "$4"; then
 
-    CONFIG="/etc/opendomo/system/exports"
+    # Adding drive to export
+    NFSCONFIG="/etc/opendomo/system/exports"
     DRIVE="/media/$4"
     OPTIONS="rw,no_root_squash,subtree_check"
     IPRANGE=`hostname -I | awk -F. '{print$1"."$2"."$3}'`
     HOSTRANGE=`echo $IPRANGE.0/255.255.255.0`
-    SERVICES="nfs-kernel-server"
 
-    # Adding drive to export
-    if [ `grep -n1 -c "$4" $CONFIG` == "1" ]; then
+    if [ `grep -n1 -c "$4" $NFSCONFIG` == "1" ]; then
         echo "#ERRO Drive exist"
     else
         echo "#INFO Adding drive"
         echo "/media/$4 $HOSTRANGE($OPTIONS)" >> "$CONFIG"
 
         # Restarting services
-        sudo changestate.sh service $SERVICES off
-        sudo changestate.sh service $SERVICES on
+        sudo changestate.sh service nfs-kernel-server off
+        sudo changestate.sh service nfs-kernel-server on
+    fi
+    # Adding drive to samba configuration file
+    SMBCONFIG="/etc/opendomo/system/smb.conf"
+    NAME="$4"
+    ROUTE="/media/$4"
+
+    if [ `grep -n1 -c "\[ $4 \]" $SMBCONFIG` == "1" ]; then
+        echo "#ERRO Drive exist"
+    else
+        echo "#INFO Adding drive"
+        echo                                      >> $SMBCONFIG
+        echo "[ $4 ]"				  >> $SMBCONFIG
+        echo "     comment = $4 Shared by samba"  >> $SMBCONFIG
+        echo "     path = $ROUTE"                 >> $SMBCONFIG
+        echo "     writeable = yes"               >> $SMBCONFIG
+        echo "     guest ok = no"                 >> $SMBCONFIG
+        echo "     public = yes"                  >> $SMBCONFIG
+
+        # Restarting services
+        sudo changestate.sh service samba off
+        sudo changestate.sh service samba on
     fi
 else
     echo "#ERRO You need specify drive to share"
